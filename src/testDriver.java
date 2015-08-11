afcf024e6b1066f47ecaeae51b38dd0be3618758
uvrd <brion.rogers@gmail.com>
2015-08-11 10:33:16 -0500

    Update testDriver.java

--- src/testDriver.java
+++ src/testDriver.java
@@ -10,6 +10,5 @@ public class testDriver
 		object.scrapeSite();
 		urlObject object1 = new urlObject("http://www.amazon.com/Cuckoos-Calling-Robert-Galbraith/dp/0316206849/ref=sr_1_1_title_1_har?s=books&ie=UTF8&qid=1396249063&sr=1-1&keywords=jk+rowling");
 		object1.scrapeSite();
-		System.out.println("Yo");
 	}
 }
